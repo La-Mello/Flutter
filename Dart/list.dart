3d0e75dd0ef55ef0e72bb6d99d2165188e278e3d
star <juniorprogrammer09@gmail.com>
2022-11-18 13:01:32 +0300

    initial demonstration<with errors>

---
+++ Dart/list.dart
@@ -0,0 +1,35 @@
+/**
+    an implementation of lists in dart
+    lists are the arrays representation in dart
+    !Note:we dont have arrays in dart
+
+    There are two types of lists:
+        Fixed list - have fixed sizes
+        Growable list- the size can grow
+ */
+
+ void main(){
+
+    //! fixed lists 
+    // size is declared during initialization
+
+    // a list to store five elements
+    //syntax to declare a list
+    //! var lst=new List(size);
+    var nums = new List();
+
+    //elements can be accessed using indexes
+    nums[0]=10;
+    nums[1]=20;
+    nums[2]=30;
+    nums[3]=40;
+    nums[4]=50;
+
+    //! printing the list
+    for (int i=0;i<5;i++)
+        print("${nums[i]}");
+    print(nums);
+    
+
+
+ }
